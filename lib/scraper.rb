a7d06efd04a3236963529200f31298aed33bc480
Erin Stansbury <ekstansbury@gmail.com>
2020-04-04 02:46:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,10 +34,8 @@ class Scraper
         student_profile[:blog] = social.attribute("href").value
       end
     end
-
     student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
     student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
-
     student_profile
   end
 
